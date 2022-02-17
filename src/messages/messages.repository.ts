d42bda539ab292dc3b9448151d152b9009f08700
Ifycode <marychord@gmail.com>
2022-02-18 00:28:40 +0100

    Add: Implementing a repository

---
+++ src/messages/messages.repository.ts
@@ -0,0 +1,17 @@
+import { readFile } from 'fs/promises';
+
+export class MessagesRepository {
+  async findOne(id: string) {
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents);
+    return messages[id];
+  }
+
+  async findAll() {
+
+  }
+
+  async create(message: string) {
+
+  }
+}
