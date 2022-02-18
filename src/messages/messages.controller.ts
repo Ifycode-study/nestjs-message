426565ebe3d81a37365ea3f339b0b07842af49e0
Ifycode <marychord@gmail.com>
2022-02-18 01:30:41 +0100

    Add: Reporting errors with exceptions

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
 import { MessagesService } from './messages.service';
 
@@ -23,7 +30,11 @@ export class MessagesController {
   }
 
   @Get('/:id')
-  getMessage(@Param('id') id: string) {
-    return this.messagesService.findOne(id);
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+    if (!message) {
+      throw new NotFoundException('message not found!');
+    }
+    return message;
   }
 }
