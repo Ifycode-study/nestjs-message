4c539dca976921c26f13ebebc843499150777455
Ifycode <marychord@gmail.com>
2022-02-18 02:56:07 +0100

    Add: Refactoring to use dependency injection

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -11,13 +11,7 @@ import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
-  messagesService: MessagesService;
-
-  constructor() {
-    // Service is creating it's own dependencies here
-    // DON'T DO THIS IN REAL APPS (use dependency injection instead)
-    this.messagesService = new MessagesService();
-  }
+  constructor(public messagesService: MessagesService) { }
 
   @Get()
   listMessages() {
--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,13 +1,9 @@
+import { Injectable } from '@nestjs/common';
 import { MessagesRepository } from './messages.repository';
 
+@Injectable()
 export class MessagesService {
-  messagesRepo: MessagesRepository;
-
-  constructor() {
-    // Service is creating it's own dependencies here
-    // DON'T DO THIS IN REAL APPS (use dependency injection instead)
-    this.messagesRepo = new MessagesRepository();
-  }
+  constructor(public messagesRepo: MessagesRepository) { }
 
   findOne(id: string) {
     return this.messagesRepo.findOne(id);
